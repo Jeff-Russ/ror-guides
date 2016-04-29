aa66f349d6b1657c13c9e89f793d6053350d1c9c
Jeff Russ <jeffreylynnruss@gmail.com>
2016-04-28 23:28:03 -0400

    LOTS added

--- school_example/app/controllers/teachers_controller.rb
+++ school_example/app/controllers/teachers_controller.rb
@@ -8,17 +8,60 @@ class TeachersController < ApplicationController
   end
 
   def new
+    @teacher = Teacher.new
+    puts "### in teachers#new"
   end
 
   def edit
   end
 
   def create
+    puts "### in teachers#create"
+    puts "#{params}"
+    # instantiate a (unsaved) teacher object
+    # with mass-assigment and whitelisting:
+    @teacher = Teacher.new(teacher_params)
+
+    if @teacher.save # <- save w/ test
+      flash[:notice] = "New teacher added!"
+      # success! send user to teacher index:
+      redirect_to(action: 'index')
+      puts "Save successful!"
+    else
+      # falure. send user back to form.
+      # (where @teacher can prepopulate)
+      render('new')
+    end
+  end
+
+  def update
+    # find teacher being updated: 
+    @teacher = Teacher.find(params[:id])
+
+    if @teacher.update_attributes(teacher_params)
+      flash[:notice] = "Teacher updated!"
+      # success! send user to teacher SHOW page:
+      redirect_to(action: 'show', id:  @teacher.id ) 
+    else
+      # falure. send user back to EDIT form.
+      # (where @teacher can prepopulate)
+      render('edit')
+    end
   end
   
-  def update
+  def delete
+    @teacher = Teacher.find(params[:id])
   end
   
   def destroy
+    flash[:notice] = "Teacher deleted!"
+    @teacher = Teacher.find(params[:id])
+    @teacher.destroy
+    redirect_to(action: 'index')
+  end
+
+ private
+  def teacher_params
+    params.require(:teacher).permit(:first_name, :last_name)
   end
 end
--- school_example/config/routes.rb
+++ school_example/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root "pages#home"
   # resources :teachers
-  match ':controller(/:action(/:id))', :via => :get
+  match ':controller(/:action(/:id))', :via => [:get, :post]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
